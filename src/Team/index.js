2de1b7270bdd6bbe980ea3344bde3a305edf8b0e
krishiv <krishivpiduri337@gmail.com>
2024-10-19 19:35:59 -0500

    Added Krishiv Image

--- src/Team/index.js
+++ src/Team/index.js
@@ -1,15 +1,17 @@
 import "./PersonCard";
 import PersonCard from "./PersonCard";
 import img from "./sample.jpg";
+import aarush from "./Aarush_Tulsyan.png"
+import krishiv from "./KrishivPiduri.jpg";
 export default function Team () {
     return (
-        <section className="bg-white dark:bg-gray-900">
+        <section className="bg-gray-900">
+            <h3 className="text-center text-white mt-20 text-4xl font-bold">Our Awesome Team</h3>
             <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6 flex-col grid gap-4 grid-cols-3">
-                <PersonCard Name={"Krishiv Piduri"} Role={"Head Organizer"} Image={img}/>
-                <PersonCard Name={"Aarush Tulsyan"} Role={"Mentor"} Image={img}/>
-                <PersonCard Name={"Shiv Mahajan"} Role={"Co Presentation Builder"} Image={img}/>
-                <PersonCard Name={"Arjun Averneni"} Role={"Co Presentation Builder"} Image={img}/>
-                <PersonCard Name={"Adwik Rath"} Role={"Did stuff too (probably)"} Image={img}/>
+                <PersonCard Name={"Krishiv Piduri"} Role={"Founder and CEO"} Image={krishiv}/>
+                <PersonCard Name={"Aarush Tulsyan"} Role={"Mentor"} Image={aarush}/>
+                <PersonCard Name={"Shiv Mahajan"} Role={"Co-CMO"} Image={img}/>
+                <PersonCard Name={"Arjun Averneni"} Role={"Co-CMO"} Image={img}/>
             </div>
         </section>
 );
