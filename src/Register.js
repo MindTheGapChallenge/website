f84b800b3987f4c14f9f0275c6787d5161ad166d
krishiv <krishivpiduri337@gmail.com>
2024-10-25 07:53:57 -0500

    Opened up Registration

--- src/Register.js
+++ src/Register.js
@@ -2,8 +2,7 @@ function Register () {
     return (
         <>
             <h1 className="m-6 text-center text-4xl font-extrabold text-color font-mono">Ready to jump in?</h1>
-            <p className="text-center dark:text-color text-color font-mono">We don't have a formal registration, yet, but you can fill out this interest form to show that you are interested. This also helps us prove that this is a legit event with people interested.</p>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdBnEvjBQH8j3lFULXIhAi1ZHVWRGgiXLpEFt8frvgkl1YeEQ/viewform?embedded=true" width="640" height="1262" frameborder="0" marginheight="0" marginwidth="0" id="interest-form" scrolling="no" className="text-white">Loading…</iframe>
+            <p className="text-center dark:text-color text-color font-mono">Are you ready to register for MindTheGap? You can go to our Devpost <a href={"https://mind-the-gap.devpost.com/"} className="text-blue-600 underline">here</a>.</p>
         </>
     );
 }
