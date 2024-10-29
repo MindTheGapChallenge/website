ff25f6dc950724b20d0da1034213a8e617a19682
krishiv <krishivpiduri337@gmail.com>
2024-10-29 05:58:12 -0500

    Updated src/Team/index.js

--- src/Team/index.js
+++ src/Team/index.js
@@ -2,7 +2,6 @@ import "./PersonCard";
 import PersonCard from "./PersonCard";
 import aarush from "./Aarush_Tulsyan.png"
 import krishiv from "./KrishivPiduri.jpg";
-import sample from "./sample.png"
 export default function Team () {
     return (
         <section className="bg-gray-900">
@@ -10,7 +9,6 @@ export default function Team () {
             <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-6 flex-col grid gap-4 grid-cols-3">
                 <PersonCard Name={"Krishiv Piduri"} Role={"Founder and CEO"} Image={krishiv}/>
                 <PersonCard Name={"Aarush Tulsyan"} Role={"Mentor"} Image={aarush}/>
-                <PersonCard Name={"Shiv Mahajan"} Role={"CMO"} Image={sample}/>
             </div>
         </section>
 );
